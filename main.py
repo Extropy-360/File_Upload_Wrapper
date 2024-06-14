3fecf0c48d5992fb28e129002d6727045d69e007
Nathan <51066514+goosebruce@users.noreply.github.com>
2024-06-14 06:17:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from typing import List, Dict, Optional
 import logging
 import os
 
-class Portal:
+class FileExchange:
     '''
-        This class is used to interact with the client file portal. User will be logged in on class initiallization.
+        This class is used to interact with the client file exchange. User will be logged in on class initiallization.
 
         Attributes:
-            base_url (str): The base url of the client file portal.
+            base_url (str): The base url of the client file file_exchange.
             user_name (str): The user name of the account.
             password (str): The password of the account.
             log_level (str, optional): The logging level ('debug' or 'info'). Defaults to 'info'.
@@ -18,14 +18,14 @@ class Portal:
             upload(file_paths, upload_to_path): Uploads files to a specified path on the cloud system.
 
         Example:
-            >>> portal_client = Portal('https://files.portalsite.com', 'your_username', 'your_password')
-            >>> portal_client.upload(['/path/to/file.txt', 'relative_path.csv'], 'path/on/portal')
+            >>> upload_client = FileExchange('https://files.file_exchangesite.com', 'your_username', 'your_password')
+            >>> upload_client.upload(['/path/to/file.txt', 'relative_path.csv'], 'path/on/fileexchange')
     '''
     def __init__(self,base_url: str, user_name: str, password: str, log_level: str = 'info', user_agent: Optional[str] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'):
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(
             level=logging.DEBUG if log_level == 'debug' else logging.INFO,
-            format='%(asctime)s - Portal - %(levelname)s - %(message)s',
+            format='%(asctime)s - file_exchange - %(levelname)s - %(message)s',
             errors='ignore',
         )
         self.session = requests.Session()
@@ -38,7 +38,7 @@ class Portal:
 
     def _login(self):
         '''
-                An internal method to authenticate the user with the Portal.
+                An internal method to authenticate the user with the file exchange.
                 This method is automatically called during initialization.
         '''
         self.logger.debug(f'Atempting to login to {self.base_url} as user: {self.user_name}')
@@ -63,11 +63,11 @@ class Portal:
 
     def upload(self, file_paths: List[str], upload_to_path: 'str'):
         '''
-            Uploads files to a specified path on the client file portal.
+            Uploads files to a specified path on the client file exchange.
 
             Args:
                 file_paths (list of str): A list of local file paths to upload.
-                upload_to_path (str): The destination path on the client file portal, you can get this by navigating to the folder on the portal and copying the path from the side bar.
+                upload_to_path (str): The destination path on the client file exchange, you can get this by navigating to the folder on the file_exchange and copying the path from the side bar.
 
             Returns:
                 list of dict: A list of dictionaries containing the status of each file upload.
@@ -75,9 +75,9 @@ class Portal:
             Example:
                 >>> upload_path = '/SHARED/folder/second_folder'
                 >>> files_to_upload = ['/path/to/file.txt','test.csv', 'file_other.json']
-                >>> base_url = 'https://files.fake_portal.com'
-                >>> portal_client = Portal(base_url, 'your_username', 'your_password')
-                >>> status = portal_client.upload(file_paths=files_to_upload, upload_to_path=upload_path)
+                >>> base_url = 'https://files.fake_file_exchange.com'
+                >>> upload_client = FileExchange(base_url, 'your_username', 'your_password')
+                >>> status = upload_client.upload(file_paths=files_to_upload, upload_to_path=upload_path)
 
                 returns: [{'file_path': '/path/to/file.txt', 'file_name': 'file.txt', 'status': 'success'},{'file_path': '/path/to/relative_path.csv', 'file_name': 'relative_path.csv', 'status': 'success'},{'file_path': '/path/to/not_a_file.csv', 'file_name': 'not_a_file.csv', 'status': 'failed'}]
         '''
