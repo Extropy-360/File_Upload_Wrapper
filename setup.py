a0627912153d0a67757405209199d5c35d133e86
Nathan <51066514+goosebruce@users.noreply.github.com>
2024-06-14 14:47:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="File_Upload_Wrapper",
-    version="0.1.0",
+    version="0.2.0",
     description="A wrapper for uploading files to the portal",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Extropy-360/File_Upload_Wrapper",
-    packages=find_packages(),
+    py_modules=["file_upload_wrapper"],
     install_requires=[
         "requests",
     ],
